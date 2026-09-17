97197b4f4a0a9afc043055a8f51953a55b3084f3
kiyar <muhammadkiyar31@gmail.com>
2026-09-17 13:07:57 +0300

    Fix static file serving

--- server.js
+++ server.js
@@ -12,11 +12,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const publicPath = path.join(__dirname);
-
-app.use(express.static(publicPath));
 app.use(express.static(__dirname));
-
 const studentSchema = new mongoose.Schema(
     {
         studentId: {
